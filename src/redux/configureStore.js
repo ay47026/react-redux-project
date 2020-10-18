b63798ec08be776cf6f29f51d0e3b97e16d7a2bf
ay47026 <ay47026@gmail.com>
2020-10-18 20:16:37 +0530

    last

---
+++ src/redux/configureStore.js
@@ -0,0 +1,26 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comment';
+import { Leaders } from './leaders';
+import { Promotions } from './promotions';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+
+
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        combineReducers({
+
+            dishes: Dishes,
+            comments: Comments,
+            promotions:Promotions,
+            leaders:Leaders
+           
+        }),
+        applyMiddleware(thunk, logger)
+    );
+
+    return store; 
+}
